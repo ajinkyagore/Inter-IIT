2c411ea4411402ce4e38c97ee4f20f4810a24b5f
Vikrant Dey <46784336+OhmVikrant@users.noreply.github.com>
2019-12-04 15:57:50 +0530

    Add files via upload

---
+++ communication/drone1.py
@@ -0,0 +1,30 @@
+from time import sleep
+from firebase import firebase
+
+
+import urllib2, urllib, httplib
+import json
+
+firebase = firebase.FirebaseApplication('https://inter-iit-drone-2020.firebaseio.com/', None)
+str_gps="23.34566,78.45766"
+
+count1=0
+obj_detected=True  # if object is detected this value goes True
+def update_firebase(str_gps,count):
+    
+    
+    lat=float(str_gps.split(',')[0])
+    lon=float(str_gps.split(',')[1])
+    firebase.put('/drone1/obj'+str(count1),"lat",lat)
+    firebase.put('/drone1/obj'+str(count1),"lng",lon)
+    firebase.put('/',"count1",count1)   
+    
+while True:
+    if obj_detected:
+        count1 += 1
+        update_firebase(str_gps,count1)#only if object detected
+    count_total=count1+firebase.get('/count2',None)+firebase.get('/count3',None)#run forever
+    firebase.put('/',"count",count_total)#run forever
+    # is_count_full(count_total)
+        # this function will check if total count is > 5 -> RTL
+    sleep(1)
---
+++ communication/drone3.py
@@ -0,0 +1,33 @@
+from time import sleep
+from firebase import firebase
+
+
+import urllib2, urllib, httplib
+import json
+#from functools import partial
+
+firebase = firebase.FirebaseApplication('https://inter-iit-drone-2020.firebaseio.com/', None)
+str_gps="23.34566,78.45766"
+
+count=0
+obj_detected=True
+def update_firebase(str_gps,count):
+    
+    
+    lat=float(str_gps.split(',')[0])
+    lon=float(str_gps.split(',')[1])
+    firebase.put('/drone3/obj'+str(count),"lat",lat)
+    firebase.put('/drone3/obj'+str(count),"lng",lon)
+    
+    
+    firebase.put('/',"count3",count)
+    
+    
+while True:
+    if obj_detected:
+        count += 1
+        update_firebase(str_gps,count)
+        sleep(1)
+    count_total=firebase.get('/count',None)
+    # is_count_full(count_total)
+        # this function will check if total count is > 5 -> RTL
